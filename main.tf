46d21ab43931b5fa069476044e9756cf0d7f16d4
wcywong69 <wcywong69@gmail.com>
2025-07-08 21:53:33 +0800

    Corrected Region again again in main.tf files

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-#   region = "ap-southeast-1"
-  region = "us-west-2"
+  region = "ap-southeast-1"
+#   region = "us-west-2"
 }
 
 terraform {
   backend "s3" {
     bucket = "my-terraform-state-bucket"
     key    = "terraform/state"
-    # region = "ap-southeast-1"
-    region = "us-west-2"
+    region = "ap-southeast-1"
+    # region = "us-west-2"
   }
 }
